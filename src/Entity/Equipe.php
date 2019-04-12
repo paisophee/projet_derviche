9a4de810ff0df8acabc197fc8e346b3136bdee3a
na ly <na-ly@live.fr>
2019-04-12 11:08:27 +0200

    création entité Equipe

---
+++ src/Entity/Equipe.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EquipeRepository")
+ *
+ * validation: contrainte d'unicité,verifier que l'email n'existe déjà pas dans la bdd
+ * @UniqueEntity(fields={"email"}, message="Il existe déjà un utilisateur avec cet email")
+ */
+class Equipe implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     *
+     * @Assert\NotBlank(message="Le nom est obligatoire")
+     * @Assert\Length(max="50", maxMessage="Le nom ne doit pas faire plus de {{ limit }} caractères")
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     *
+     * @Assert\NotBlank(message="Le prénom est obligatoire")
+     * @Assert\Length(max="50", maxMessage="Le prénom ne doit pas faire plus de {{ limit }} caractères")
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     *
+     * @Assert\NotBlank(message="L'email est obligatoire")
+     * @Assert\Email(message="L'email n'est pas valide")
+     *
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mdp;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $statut = 'ROLE_USER';
+
+    /**
+     * Mot de passe en clair pour interagir avec le formulaire d'inscription
+     * @var string
+     *
+     * @Assert\NotBlank(message="Le mot de passe est obligatoire")
+     */
+    private $plainPassword;  // = mot de passe en clair
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(string $mdp): self
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getPlainPassword(): string
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $plainPassword
+     * @return Equipe
+     */
+    public function setPlainPassword(string $plainPassword): Equipe
+    {
+        $this->plainPassword = $plainPassword;
+        return $this;
+    }
+
+    /***************** INSCRIPTION/CONNEXION *******************/
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return ['ROLE_USER'];
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return [$this->statut];
+    }
+
+    /**
+     * Returns the password used to authenticate the user.
+     *
+     * This should be the encoded password. On authentication, a plain-text
+     * password will be salted, encoded, and then compared to this value.
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->mdp;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
