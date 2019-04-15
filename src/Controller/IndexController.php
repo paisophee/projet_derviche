2a8dd9bf704b1453026cb71de8a9fc00803d00f9
stevenParis <contact@sbcom.fr>
2019-04-15 14:58:43 +0200

    resolution probleme base.html

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,6 +12,6 @@ class IndexController extends AbstractController
      */
     public function index()
     {
-        return $this->render('admin/accueil/index.html.twig');
+        return $this->render('index/index.html.twig');
     }
 }
